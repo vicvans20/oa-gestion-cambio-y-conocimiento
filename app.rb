793624c8149ef80b7f53bbfdd3b95bd5b1e3e4aa
vicvans20 <vicvans20@gmail.com>
2016-08-30 21:56:21 -0600

    Create all test activities.

--- app.rb
+++ app.rb
@@ -55,3 +55,102 @@ get '/acerca_de' do
   @prev = "/ch_7"
   erb :about
 end
+
+get '/test' do
+  @hide_next = true
+  erb :test_intro
+end
+
+# Quiz about Change Management
+get '/eval_1' do
+  @hide_next = true
+  @hide_prev = true
+  erb :eval_1
+end
+
+# Drag and drop
+post '/eval_2' do
+  cookies[:eval_1] = eval_1(params)
+  erb :eval_2 , layout: false
+end
+
+# Alphabet Soup
+post '/eval_3' do
+  cookies[:eval_2] = eval_2(params)
+  erb :eval_3, layout: false
+end
+
+# True and False
+post '/eval_4' do
+  cookies[:eval_3] = eval_3(params)
+  erb :eval_4, layout: false
+end
+
+post '/eval_final' do
+  cookies[:eval_4] = eval_4(params[:tinput].to_i)
+  cookies[:eval_4_time] = params[:tinput]
+  @results = eval_final(cookies[:eval_1].to_i, cookies[:eval_2].to_i, cookies[:eval_3].to_i ,cookies[:eval_4].to_i)
+  erb :eval_result, layout: false
+end
+
+# --------------------------------------
+# Evaluation Functions
+def eval_1(data)
+  points = 0
+  points +=1 if data[:selectionOne] == "option2"
+  points +=1 if data[:selectionTwo] == "option2"
+  points +=1 if data[:selectionThree] == "option3"
+  points +=1 if data[:selectionFour] == "option2"
+  points +=1 if data[:selectionFive] == "option1"
+  return points
+end
+
+def eval_2(data)
+  points = 0
+  points +=1 if data[:drop_1] == "4"
+  points +=1 if data[:drop_2] == "2"
+  points +=1 if data[:drop_3] == "1"
+  points +=1 if data[:drop_3] == "5"
+  points +=1 if data[:drop_3] == "3"
+  return points
+end
+
+def eval_3(data)
+  points = 0
+  points +=1 if data[:selectionOne] == "option3"
+  points +=1 if data[:selectionTwo] == "option3"
+  points +=1 if data[:selectionThree] == "option2"
+  points +=1 if data[:selectionFour] == "option1"
+  return points
+end
+
+def eval_4(segs)
+  case segs
+  when 0..120
+    points = 6
+  when 121..240
+    points = 3
+  else
+    points = 1
+  end
+  return points
+end
+
+
+
+def eval_final(ev1, ev2, ev3, ev4)
+  total_p = ev1 + ev2 + ev3 + ev4
+  case total_p
+  when 20
+    result = "A - Puntaje Perfecto!"
+  when 15..19
+    result = "B - Bien Hecho!."
+  when 10..14
+    result = "C - Hay que esforzarnos más."
+  when 5..9
+    result = "D - Deficiente"
+  else
+    result = "E - Reprobado.."
+  end
+  return result
+end
---
+++ public/js/eval.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  // Set event and ajax action for evaluation form
+  // receive the form id as string and target url to render next page
+  //  after succesfull post
+  send_eval_conf = function(form, target_url){
+    $(form).on("submit",function(event){
+      event.preventDefault();
+      console.log("inside");
+      $.ajax({
+        url: target_url,
+        type: "POST",
+        data: $(form).serialize(),
+        success: function(data){
+          // $("#layout_cont").fadeOut(400);
+          $("#eval_cont").html(data);
+          
+          // $("#layout_cont").fadeIn(300);
+        }
+      })
+    });
+  }
+  
+  send_eval_conf("#form_eval_1", "/eval_2");
+})
